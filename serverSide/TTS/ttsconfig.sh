75278b6e0614d0e2e1f5f43da9956df5d4618ec9
straydog-1212 <leulzerihun2001@gmail.com>
2024-04-02 21:07:44 -0700

    upgrades people updgrades

--- serverSide/TTS/ttsconfig.sh
+++ serverSide/TTS/ttsconfig.sh
@@ -2,3 +2,10 @@ tts --text "Text for TTS" \
     --model_name tts_models/multilingual/multi-dataset/xtts_v2 \
     --vocoder_name vocoder_models/universal/libri-tts/wavegrad \
     --out_path /home/lulu/githubApps/PersonalProjects/pythonVirtualEnviroment/practiceEnviroment
+
+
+# figure out variables to set for modelname vocoder outpath and set up config yaml file
+#tts --text "Text for TTS" \
+#    --model_name "<type>/<language>/<dataset>/<model_name>" \
+#    --vocoder_name "<type>/<language>/<dataset>/<model_name>" \
+#    --out_path folder/to/save/output/
--- serverSide/installScriptDebian.sh
+++ serverSide/installScriptDebian.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+# Prompt user for sudo password
+echo "Please enter your sudo password:"
+sudo -v
 
-sudo apt install ffmpeg
-sudo apt install libwebrtc-audio-processing1
-# Create directory for the project
-mkdir -p ~/nassieAI
-cd ~/nassieAI
+# prompts user for sudo privelages and then installs dependencies
+if [ $? -eq 0 ]; then
+	export DEBIAN_FRONTEND = noninteractive
 
-# Download and run OLLAMA installation script
-curl -fsSL https://ollama.com/install.sh | sh
+	# Installs dependencies on apt
+	sudo apt install -y ffmpeg libwebrtc-audio-processing1 python3 
 
-# Set up virtual environment for Python
-python -m venv ~/githubApps/PersonalProjects/pythonVirtualEnvironment
+	# Download and run OLLAMA installation script
+	curl -fsSL https://ollama.com/install.sh | sh
 
-# Activate the virtual environment
-source ~/githubApps/PersonalProjects/pythonVirtualEnvironment/bin/activate
+	# Exits sudo
+	sudo -k
+else
+    echo "Authentication failed. Exiting."
+    exit 1
+fi
+
+	# Create directory for the project
+	mkdir -p ~/nassieAI
+	cd ~/nassieAI
+
+	# Set up virtual environment for Python
+	python -m venv ~/nassieAI
+
+	# Activate the virtual environment
+	source ~/nassieAI/bin/activate
 
 # Install dependencies using pip
 pip install tts
