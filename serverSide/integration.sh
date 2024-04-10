3c9cc03f22010e882f50bdb835ed051c0b76be12
straydog-1212 <leulzerihun2001@gmail.com>
2024-04-09 17:49:30 -0700

    i added the json file and linked the variables to it. i still need to link the bash script json source to the actual file though

--- serverSide/integration.sh
+++ serverSide/integration.sh
@@ -1,14 +1,25 @@
 #! bin/bash/
+json_file = serverSideConfig.json
+modelname=$(jq -r '.ttsModelName' $json_file)
+lang=$(jq -r '.language' $json_file)
+threads=$(jq -r '.threads' $json_file)
+vocodername=$(jq -r '.vocoderModelName' $json_file)
+outpath=$(jq -r '.outputPath' $json_file)
 
-whisper  --language en clip_0002.ogg > friendtyler.txt
+
+whisper  --language "$lang" webrtcOuPut.ogg > whisperOutPut.txt
 # for language use "none" to do language detection
 # use the flag --threads to specify thread count of cpu to be used for whisper
 
 # cleans up whisper file to get rid of the time stamps
-sed -i 's/\[[0-9:.]* --> [0-9:.]*\]//g' speech.txt
+sed -i 's/\[[0-9:.]* --> [0-9:.]*\]//g' whisperOutPut.txt
 
 # ollama reads file output by whisper and uses it to do whatever process was required
-ollama run <modelname> "<prompt>" > ollamaoutputtext.txt
+ollama run "$modelname" "$cat whisperOutPut.txt" > ollamaOutPut.txt
 
 #text to speech reads ollamas file  and outputs an audio file
-tts --text "$(cat ollamaoutput.txt)"
+tts --text "$(cat ollamaoutput.txt)" \
+    --model_name "$modelname" \
+    --vocoder_name "$vocodername" \
+    --out_path "$outpath"
+
